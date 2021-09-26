f4405018d5bce8f5b5502b94ba6b4d02d4075acc
norito <noripmark@gmail.com>
2021-09-26 16:15:17 +0900

    first commit

---
+++ numpyarray.py
@@ -0,0 +1,14 @@
+import numpy as np
+x = np.array([[1, 2], [3, 4],[5, 6]])
+
+print(x)
+print(x[0])
+print(x[0][1])
+
+print("============")
+
+for row in x:
+    print(x)
+
+x1 = x.flatten()
+print(x)
